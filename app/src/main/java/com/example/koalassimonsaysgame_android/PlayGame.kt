50833045f6bd88ffafc74f837c32ee3da376c523
yuikonno <>
2020-12-12 14:37:24 +0900

    PlayGameActivityの追加。

---
+++ app/src/main/java/com/example/koalassimonsaysgame_android/PlayGame.kt
@@ -0,0 +1,13 @@
+package com.example.koalassimonsaysgame_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_game)
+
+
+    }
+}
