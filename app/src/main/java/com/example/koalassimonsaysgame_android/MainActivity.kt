e03fddb067de70e03b1def0cda77713787d7858b
yuikonno <>
2020-12-13 20:23:39 +0900

    GameStartボタンで、PlayGameのページに遷移させる。

--- app/src/main/java/com/example/koalassimonsaysgame_android/MainActivity.kt
+++ app/src/main/java/com/example/koalassimonsaysgame_android/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.koalassimonsaysgame_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        transitToPlayGamePage()
+
+    }
+
+    private fun transitToPlayGamePage() {
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener() {
+            val intent = Intent(this, PlayGame::class.java)
+            startActivity(intent)
+        }
     }
 }
