a412ca051b5808034dcc8464a3914fc6268fd420
yuikonno <>
2020-12-15 22:01:13 +0900

    一旦totalScoreを表示するtextを配置する。

--- app/src/main/java/com/example/koalassimonsaysgame_android/TotalScoreActivity.kt
+++ app/src/main/java/com/example/koalassimonsaysgame_android/TotalScoreActivity.kt
@@ -2,10 +2,14 @@ package com.example.koalassimonsaysgame_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class TotalScoreActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_total_score)
+
+        val scoreText = intent.getStringExtra("totalScore")
+        findViewById<TextView>(R.id.totalScoreText).text = "あなたの得点は\n${scoreText}点です。"
     }
 }
