6d9ebe07703914b4a361d530e31ef5eaa1850b5a
yuikonno <>
2021-01-09 16:03:52 +0900

    RankingActivityを作成する。

---
+++ app/src/main/java/com/yui/koalassimonsaysgame_android/rankingPage/RankingActivity.kt
@@ -0,0 +1,12 @@
+package com.yui.koalassimonsaysgame_android.rankingPage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yui.koalassimonsaysgame_android.R
+
+class RankingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ranking)
+    }
+}
