d1ab593d979247725a8b138a0c9d61e8fca06c62
yuikonno <>
2021-02-04 21:54:22 +0900

    Adapterのコードの書き方変更してみる。

--- app/src/main/java/com/yui/koalassimonsaysgame_android/rankingPage/RecyclerListAdapter.kt
+++ app/src/main/java/com/yui/koalassimonsaysgame_android/rankingPage/RecyclerListAdapter.kt
@@ -6,26 +6,26 @@ import androidx.recyclerview.widget.RecyclerView
 import com.yui.koalassimonsaysgame_android.R
 import com.yui.koalassimonsaysgame_android.resultPage.ResultActivity
 
-class RecyclerListAdapter(private val rankingDataList: MutableList<ResultActivity.RankingData>): RecyclerView.Adapter<RecyclerListViewHolder>() {
+class RecyclerListAdapter(private var rankingDataList: MutableList<ResultActivity.RankingData>): RecyclerView.Adapter<RecyclerListViewHolder>() {
 
     //ViewHolderクラスを使ってViewHolderを作成
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerListViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val item = layoutInflater.inflate(R.layout.recyclerview_row, parent, false)
+
         return RecyclerListViewHolder(item)
+    }
+
+    // 一行のViewに対して共通でやりたい処理をここで書く。
+    override fun onBindViewHolder(holder: RecyclerListViewHolder, position: Int) {
+        val rowData = rankingDataList[position]
+        holder.rank.text = "1位"
+        holder.name.text = rowData.rankingName
+        holder.score.text = rowData.rankingScore.toString()
     }
 
     //recyclerViewのコンテンツのサイズ
     override fun getItemCount(): Int {
         return rankingDataList.size
     }
-
-    // 一行のViewに対して共通でやりたい処理をここで書く。
-    override fun onBindViewHolder(holder: RecyclerListViewHolder, position: Int) {
-        val rowData = rankingDataList[position]
-//        holder.rank.text = rowData.rank.toString()
-        holder.name.text = rowData.rankingName
-        holder.score.text = rowData.rankingScore.toString()
-    }
-
 }
