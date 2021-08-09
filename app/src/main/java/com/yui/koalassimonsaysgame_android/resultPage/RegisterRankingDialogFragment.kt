29b00b3dd73431f6989cd16a392e73935ec62d0d
yuikonno <>
2021-08-09 17:32:34 +0900

    EditTextの紐付けができていなかったため、修正する。

--- app/src/main/java/com/yui/koalassimonsaysgame_android/resultPage/RegisterRankingDialogFragment.kt
+++ app/src/main/java/com/yui/koalassimonsaysgame_android/resultPage/RegisterRankingDialogFragment.kt
@@ -15,17 +15,18 @@ class RegisterRankingDialogFragment : DialogFragment() {
         val builder = AlertDialog.Builder(activity)
         val inflater = activity!!.layoutInflater
         val view = inflater.inflate(R.layout.activity_register_ranking_dialog_fragment, null)
-
-        val nameText = EditText(context)
-        nameText.setHint(this.getString(R.string.registerForRankingDialog_editText_hint))
-
+        
         builder.setView(view)
             .setPositiveButton(this.getString(R.string.registerForRankingDialog_positiveText), DialogInterface.OnClickListener { dialog, which ->
+
                 // OKボタン押したときの処理(rankingに登録する)
+                val nameText = view.findViewById<EditText>(R.id.editText)
+                nameText.setHint(this.getString(R.string.registerForRankingDialog_editText_hint))
+
                 val userText = nameText.getText().toString().trim()
 
                 // Fragment表示の確認のため、一旦処理はせずtoastにする。
-                Toast.makeText(activity, "登録", Toast.LENGTH_SHORT).show()
+                Toast.makeText(activity, nameText.text, Toast.LENGTH_SHORT).show()
 //                presenter.didTapRegisterButton(userText)
             })
 
